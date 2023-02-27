0b2f362f7dada3cc0ad8d3c29c235c03afaccd1b
Bak David <bak.david96@gmail.com>
2023-02-27 21:28:24 +0200

    Steve 27th birthday project setup

---
+++ test1.js
@@ -0,0 +1,8 @@
+function solve() {
+  let text = document.getElementById("testText").value;
+  if (text === 'BUZI') {
+    document.getElementById("testResult").innerHTML = '0124';
+  } else {
+
+  }
+}
